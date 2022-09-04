b0ac7ff493529173d035dcf44b294a880b9f636a
seriouskang <scdean110@gmail.com>
2022-09-04 19:41:38 +0900

    순수 JPA 기반 리포지토리 만들기

--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -1,11 +1,14 @@
 package study.datajpa.repository;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -25,5 +28,36 @@ class MemberJpaRepositoryTest {
         assertThat(foundMember.getId()).isEqualTo(member.getId());
         assertThat(foundMember.getUsername()).isEqualTo(member.getUsername());
         assertThat(foundMember).isEqualTo(member);
+    }
+
+    @Test
+    void basic_crud() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        // 단건 조회 검증
+        Member foundMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member foundMember2 = memberJpaRepository.findById(member2.getId()).get();
+
+        assertThat(foundMember1).isEqualTo(member1);
+        assertThat(foundMember2).isEqualTo(member2);
+
+        // 리스트 조회 검증
+        List<Member> all = memberJpaRepository.findAll();
+        assertThat(all).hasSize(2);
+
+        // 카운트 검증
+        long count = memberJpaRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        // 삭제 검증
+        memberJpaRepository.delete(member1);
+        memberJpaRepository.delete(member2);
+
+        long deletedCount = memberJpaRepository.count();
+        assertThat(deletedCount).isEqualTo(0);
     }
 }
